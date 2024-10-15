7c81d917478840e5b1b5a1a11df4c6ad741c9d8f
sudarsancse <sudarsansarkarcst@gmail.com>
2024-10-16 02:04:52 +0530

    Navbar page functions

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import { Route, Routes } from "react-router-dom";
-import "./App.css";
 import Home from "./Components/pages/Home";
 import Login from "./Components/pages/Login";
 import About from "./Components/pages/About";
@@ -9,19 +8,21 @@ import Contect from "./Components/pages/Contect";
 import PlaceOrder from "./Components/pages/PlaceOrder";
 import Product from "./Components/pages/Product";
 import Collection from "./Components/pages/Collection";
+import Navbar from "./Components/component/Navbar";
 
 function App() {
   return (
     <div className=" px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/about" element={<About />} />
         <Route path="/order" element={<Orders />} />
         <Route path="/cart" element={<Cart />} />
-        <Route path="/contect" element={<Contect />} />
+        <Route path="/contact" element={<Contect />} />
         <Route path="/placeorder" element={<PlaceOrder />} />
-        <Route path="/product" element={<Product />} />
+        <Route path="/product/:productId" element={<Product />} />
         <Route path="/collection" element={<Collection />} />
       </Routes>
     </div>
