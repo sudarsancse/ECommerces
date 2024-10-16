687acd1d5f7b33bc0739c145e4e5b35b48669c1f
sudarsancse <sudarsansarkarcst@gmail.com>
2024-10-16 17:38:59 +0530

    Add best seller and policy section

--- client/src/Components/pages/Home.jsx
+++ client/src/Components/pages/Home.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Hero from "../component/Hero";
 import LatestCollection from "../component/LatestCollection";
+import BestSeller from "../component/BestSeller";
+import Policy from "../component/Policy";
 
 function Home() {
   return (
     <div>
       <Hero />
       <LatestCollection />
+      <BestSeller />
+      <Policy />
     </div>
   );
 }
