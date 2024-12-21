21c5a5e1889bd2d0d2624acf9f922668086f65a2
sudarsancse <sudarsansarkarcse@gmail.com>
2024-12-21 16:31:43 +0530

    password visible option

--- admin/src/Components/Pages/Login.jsx
+++ admin/src/Components/Pages/Login.jsx
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { toast } from "react-toastify";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 
 function Login({ setToken }) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
 
   const onSubmitHandler = async (e) => {
     try {
@@ -44,14 +47,31 @@ function Login({ setToken }) {
           </div>
           <div className=" mb-3 min-w-72">
             <p className=" text-sm font-medium text-gray-700 mb-2">Password</p>
-            <input
+            {/* <input
               onChange={(e) => setPassword(e.target.value)}
               value={password}
               className=" rounded-md w-full px-3 py-2 border border-gray-300 outline-none"
               type="Password"
               placeholder=" Enter Your Password"
               required
-            />
+            /> */}
+            <div className="relative">
+              <input
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+                className="rounded-md w-full px-3 py-2 pr-10 border border-gray-300 outline-none"
+                type={showPassword ? "text" : "password"} // Conditional input type
+                placeholder="Enter Your Password"
+                required
+              />
+              <button
+                type="button"
+                onClick={() => setShowPassword(!showPassword)}
+                className="absolute top-1/2 right-3 transform -translate-y-1/2 text-gray-500 hover:text-gray-700 focus:outline-none"
+              >
+                <FontAwesomeIcon icon={showPassword ? faEyeSlash : faEye} />
+              </button>
+            </div>
           </div>
           <button
             className=" mt-2 w-full py-2 px-4 bg-black rounded-md hover:bg-green-600 text-white"
